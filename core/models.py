0c16ccb3c0924fa6be1f31001b11b80ce44a8bea
ac-hj <hyeon.j.choi00@gmail.com>
2021-06-19 10:50:50 -0700

    adding some comments

--- core/models.py
+++ core/models.py
@@ -6,4 +6,7 @@ class Diary(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now=True)
+    # this links the diary model to the user model
+    # so when you delete users, you need to delete 
+    # that user's diary entries first
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
